702a015f6791ba6f81cc0c47ee3f3a1f385d1173
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2024-01-23 23:11:39 +0100

    Updated moonfly theme

--- plugins/themes/moonfly.nix
+++ plugins/themes/moonfly.nix
@@ -5,19 +5,27 @@
   ...
 }:
 with lib; {
-	options.colorschemes.moonfly = {
-		enable = mkEnableOption "A dark charcoal theme for modern Neovim & classic Vim";
-	};
+  options.colorschemes.moonfly = {
+    enable = mkEnableOption "A dark charcoal theme for modern Neovim & classic Vim";
+  };
 
-	config = let
-		cfg = config.colorschemes.moonfly;
-	in
-		mkIf cfg.enable {
-			extraPlugins = with pkgs.vimPlugins; [vim-moonfly-colors];
+  config = let
+    cfg = config.colorschemes.moonfly;
+  in
+    mkIf cfg.enable {
+      extraPlugins = with pkgs.vimPlugins; [vim-moonfly-colors];
 
-			extraConfigLua = ''
-				vim.cmd([[colorscheme moonfly]])
-				'';
-		};
+      extraConfigLua = ''
+        -- Enable transparency
+        vim.g.moonflyTransparent = true
+        -- Underline matching parentheses
+        vim.g.moonflyUnderlineMatchParen = true
+        -- Display diagnostic virtual text in color
+        vim.g.moonflyVirtualTextColor = true
+        -- Win separator (line)
+        vim.g.moonflyWinSeparator = 2
+        vim.opt.fillchars = { horiz = '━', horizup = '┻', horizdown = '┳', vert = '┃', vertleft = '┫', vertright = '┣', verthoriz = '╋', }
+        vim.cmd([[colorscheme moonfly]])
+      '';
+    };
 }
-
