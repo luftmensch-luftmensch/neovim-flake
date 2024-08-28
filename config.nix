5f39e83396493d6fdf8af3388d6d548018b1abdc
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2024-08-28 11:13:51 +0200

    flake: bump

--- config.nix
+++ config.nix
@@ -596,34 +596,36 @@
 
       conform-nvim = {
         enable = true;
-        notifyOnError = false;
-        formatOnSave = {
-          timeoutMs = 500;
-          lspFallback = true;
-        };
+        settings = {
+          notify_on_error = false;
+          format_on_save = {
+            timeoutMs = 500;
+            lspFallback = true;
+          };
 
-        # Map of filetype to formatters
-        formattersByFt = {
-          lua = [ "stylua" ];
-          tex = [ "latexindent" ];
-          c = [ "clang-format" ]; # "astyle"
-          # Conform will run multiple formatters sequentially
-          python = [
-            "isort"
-            "black"
-          ];
-          # Use a sub-list to run only the first available formatter
-          javascript = [
-            [
-              "prettierd"
-              "prettier"
-            ]
-          ];
-          # Use the "*" filetype to run formatters on all filetypes.
-          "*" = [ "codespell" ];
-          # Use the "_" filetype to run formatters on filetypes that don't
-          # have other formatters configured.
-          "_" = [ "trim_whitespace" ];
+          # Map of filetype to formatters
+          formatters_by_ft = {
+            lua = [ "stylua" ];
+            tex = [ "latexindent" ];
+            c = [ "clang-format" ]; # "astyle"
+            # Conform will run multiple formatters sequentially
+            python = [
+              "isort"
+              "black"
+            ];
+            # Use a sub-list to run only the first available formatter
+            javascript = [
+              [
+                "prettierd"
+                "prettier"
+              ]
+            ];
+            # Use the "*" filetype to run formatters on all filetypes.
+            "*" = [ "codespell" ];
+            # Use the "_" filetype to run formatters on filetypes that don't
+            # have other formatters configured.
+            "_" = [ "trim_whitespace" ];
+          };
         };
       };
 
